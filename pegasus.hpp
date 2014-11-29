42089d458e46b9901ebbead5e427d0149dff808f
yuri_kilochek <yuri.kilochek@gmail.com>
2014-11-29 17:33:14 +0300

    Added main library header.

---
+++ pegasus.hpp
@@ -0,0 +1,34 @@
+#ifndef PEGASUS_HEADER
+#define PEGASUS_HEADER
+
+#include "pegasus/character.hpp"
+#include "pegasus/any_character.hpp"
+#include "pegasus/character_range.hpp"
+#include "pegasus/character_set.hpp"
+
+#include "pegasus/empty_string.hpp"
+#include "pegasus/string.hpp"
+
+#include "pegasus/optional.hpp"
+
+#include "pegasus/repeated.hpp"
+#include "pegasus/repeated_at_least_once.hpp"
+
+#include "pegasus/sequence.hpp"
+
+#include "pegasus/ordered_choice.hpp"
+
+#include "pegasus/not_predicate.hpp"
+#include "pegasus/and_predicate.hpp"
+
+#include "pegasus/stack_action.hpp"
+
+#include "pegasus/end_of_text.hpp"
+
+#include "pegasus/failure.hpp"
+#include "pegasus/state.hpp"
+#include "pegasus/list.hpp"
+#include "pegasus/iterator_cursor.hpp"
+#include "pegasus/parse.hpp"
+
+#endif
